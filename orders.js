36fbbf17ecd25c33d5aede6b8b76f167eece5aa9
Austin Killough <austinbkillough@gmail.com>
2023-05-01 12:44:18 -0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+// write a kintone function to update the stock of an item when an order is placed
+
+// code to be used directl in the kintone app - IIFE
+
+(function() {
+  'use strict';
+  // let item = record.record.item_lookup.value;
+  function test(event){
+    event.record.qty.error = 'You suck';
+    return event;
+  }
+  function changeStock (event) {
+    const order_type = event.record.order_type.value;
+    const lookup = event.record.item_lookup.value;
+    const currentStock = event.record.current_stock.value
+    const quantity = event.record.qty.value
+    const endQty = order_type === 'Sale' ? currentStock - quantity : Number(currentStock) + Number(quantity);
+    
+    const body = {
+      'app': 6,
+      'updateKey': {
+        'field': 'item_code',
+        'value': lookup
+      },
+      'record': {
+        'stock': {
+          'value': endQty
+        },
+      }
+    };
+    kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', body, function(response) {
+  // success
+  console.log('response: ', response);
+}, function(error) {
+  // error
+  console.log(error);
+});
+  }
+  kintone.events.on('app.record.detail.process.proceed', function(event) {
+    if (event.nextStatus.value === 'In progress') {
+      changeStock(event);
+    }
+  });
+})();
